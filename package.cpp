eb27d90bdc31bdd5fb6e9bf9e40ca13658666fb8
Bomby888 <115494214+Bomby888@users.noreply.github.com>
2025-12-27 16:47:23 +0100

    Struktura plików-testy OK
    Dodano klasy Package oraz PackageQueue. Testy co do nich wszystkie przechodzą. zmodyfikowano cmake ponownie, dodano komentarz co do instalacji

--- package.cpp
+++ package.cpp
@@ -1,1 +1,63 @@
 #include "package.hxx"
+#include <stdexcept>
+
+// pola statyczne
+std::set<ElementID> Package::assigned_ids_;
+std::set<ElementID> Package::freed_ids_;
+
+//pomocnicze funkcje
+ElementID Package::generate_id() {
+    if (!freed_ids_.empty()) {
+        auto it = freed_ids_.begin();
+        ElementID id = *it;
+        freed_ids_.erase(it);
+        assigned_ids_.insert(id);
+        return id;
+    }
+
+    ElementID new_id = assigned_ids_.empty() ? 1 : (*assigned_ids_.rbegin() + 1);
+    assigned_ids_.insert(new_id);
+    return new_id;
+}
+
+//kontruktory
+Package::Package()
+    : id_(generate_id()) {}
+
+Package::Package(ElementID id)
+    : id_(id) {
+    assigned_ids_.insert(id_);
+}
+
+Package::Package(Package&& other) noexcept
+    : id_(other.id_) {
+    other.id_ = 0;
+}
+
+// operator przypisania
+Package& Package::operator=(Package&& other) noexcept {
+    if (this != &other) {
+        // zwolnij swoje ID
+        if (id_ != 0) {
+            assigned_ids_.erase(id_);
+            freed_ids_.insert(id_);
+        }
+
+        id_ = other.id_;
+        other.id_ = 0;
+    }
+    return *this;
+}
+
+// metoda
+ElementID Package::get_id() const {
+    return id_;
+}
+
+// destruktor, jeśli ID nie jest zerowe, zwalnia je
+Package::~Package() {
+    if (id_ != 0) {
+        assigned_ids_.erase(id_);
+        freed_ids_.insert(id_);
+    }
+}
--- storage_types.cpp
+++ storage_types.cpp
@@ -1,1 +1,58 @@
 #include "storage_types.hxx"
+#include <stdexcept>
+
+//PackageQueue
+PackageQueue::PackageQueue(PackageQueueType type)
+    : type_(type) {}
+
+void PackageQueue::push(Package&& package) {
+    container_.push_back(std::move(package));
+}
+
+Package PackageQueue::pop() {
+    if (container_.empty()) {
+        throw std::logic_error("PackageQueue::pop() on empty queue");
+    }
+
+    Package result = (type_ == PackageQueueType::FIFO)
+        ? std::move(container_.front())
+        : std::move(container_.back());
+
+    if (type_ == PackageQueueType::FIFO) {
+        container_.pop_front();
+    } else {
+        container_.pop_back();
+    }
+
+    return result;
+}
+
+//iteratory
+IPackageStockpile::const_iterator PackageQueue::begin() const {
+    return container_.begin();
+}
+
+IPackageStockpile::const_iterator PackageQueue::end() const {
+    return container_.end();
+}
+
+IPackageStockpile::const_iterator PackageQueue::cbegin() const {
+    return container_.cbegin();
+}
+
+IPackageStockpile::const_iterator PackageQueue::cend() const {
+    return container_.cend();
+}
+
+//info o rozmiarze
+std::size_t PackageQueue::size() const {
+    return container_.size();
+}
+
+bool PackageQueue::empty() const {
+    return container_.empty();
+}
+
+PackageQueueType PackageQueue::get_queue_type() const {
+    return type_;
+}
